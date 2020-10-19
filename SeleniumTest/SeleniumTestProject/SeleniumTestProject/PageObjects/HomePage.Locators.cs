5cec8cfcd8d4545e3c163b67c086aa3e51f60b80
KingaStuchirenea <72432661+KingaStuchirenea@users.noreply.github.com>
2020-10-19 20:51:22 +0300

    Adaugare Selenium Tests
    Adaugare Selenium Tests

---
+++ SeleniumTest/SeleniumTestProject/SeleniumTestProject/PageObjects/HomePage.Locators.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumTestProject.PageObjects
+{
+    partial class HomePage
+    {
+        private IWebDriver _driver;
+        public HomePage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+        public IWebElement AutentificationButton => _driver.FindElement(By.XPath("//a[text()='Autentificare']"));
+        public IWebElement InscriereButton => _driver.FindElement(By.XPath("//a[text()='Înscriere']"));
+    }
+}
---
+++ SeleniumTest/SeleniumTestProject/SeleniumTestProject/Tests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using SeleniumTestProject.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SeleniumTestProject
+{
+    [TestFixture]
+    class Tests : Hooks
+    {
+        [Test]
+        public void MyFirstTest()
+        {
+            HomePage myPage = new HomePage(Driver);
+            myPage.AutentificationButton.Click();
+            Thread.Sleep(1000);
+        }
+
+        [Test]
+        public void MySecondTest()
+        {
+            Thread.Sleep(1000);
+        }
+
+        [Test]
+        public void MyThirdTest()
+        {
+            Thread.Sleep(1000);
+        }
+    }
+}
