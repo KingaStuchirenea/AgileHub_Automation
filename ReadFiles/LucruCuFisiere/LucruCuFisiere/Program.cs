2db1ef90c2f597ca981217fba129e0eb46be8e51
KingaStuchirenea <72432661+KingaStuchirenea@users.noreply.github.com>
2020-11-16 20:47:52 +0200

    Curs scriere/citire fisiere
    Curs scriere/citire fisiere

---
+++ ReadFiles/LucruCuFisiere/LucruCuFisiere/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LucruCuFisiere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Adaugare linii in fisier:
+            //string[] lines = { "Linia 1", "Linia 2", "Linia 3", "Linia 4", "Linia 5" };
+            //foreach(var line in lines)
+            //{
+            //    Console.WriteLine(line);
+            //}
+            //System.IO.File.WriteAllLines(@"C:\Users\kstuchirenea\Desktop\AgileHub\NewGitHub\AgileHub_Automation\ReadFiles\LucruCuFisiere\LucruCuFisiere\WriteLinesSample.txt", lines);
+            //Console.WriteLine("Liniile au fost scrise in fisier");
+            //Console.ReadKey();
+
+            //Adaugare text in fisier:
+            //string text = "Brașov is located in the central part of the country, about 166 kilometres (103 miles) north of Bucharest and 380 kilometres (236 miles) from the Black Sea. It is surrounded by the Southern Carpathians and is part of the historical region of Transylvania.";
+            //System.IO.File.WriteAllText(@"C:\Users\kstuchirenea\Desktop\AgileHub\NewGitHub\AgileHub_Automation\ReadFiles\LucruCuFisiere\LucruCuFisiere\WriteLinesSample.txt", text);
+            //Console.WriteLine("Liniile au fost scrise in fisier");
+            //Console.ReadKey();
+
+            //Filtrare date la adaugare:
+            //string[] lines = { "Linia 1", "Linia 2", "Linia 3", "Linia 4", "Linia 5" };
+            //using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\kstuchirenea\Desktop\AgileHub\NewGitHub\AgileHub_Automation\ReadFiles\LucruCuFisiere\LucruCuFisiere\WriteLinesSample.txt"))
+            //{
+            //    foreach (var line in lines)
+            //    {
+            //        if (!line.Contains("3"))
+            //        {
+            //            file.WriteLine(line);
+            //        }
+            //    }
+            //}
+
+            //Adaugare in continuare la fisier, append
+            //string[] lines = { "Linia 1", "Linia 2", "Linia 3", "Linia 4", "Linia 5" };
+            //using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\kstuchirenea\Desktop\AgileHub\NewGitHub\AgileHub_Automation\ReadFiles\LucruCuFisiere\LucruCuFisiere\WriteLinesSample.txt",true))
+            //{
+            //    foreach (var line in lines)
+            //    {
+            //        if (line.Contains("3"))
+            //        {
+            //            file.WriteLine(line);
+            //        }
+            //    }
+            //}
+            //Console.WriteLine("Liniile au fost scrise in fisier");
+
+            //citire din fisier 1
+            //string readText = System.IO.File.ReadAllText(@"C:\Users\kstuchirenea\Desktop\AgileHub\NewGitHub\AgileHub_Automation\ReadFiles\LucruCuFisiere\LucruCuFisiere\WriteLinesSample.txt");
+            //string[] lines = System.IO.File.ReadAllLines(@"C:\Users\kstuchirenea\Desktop\AgileHub\NewGitHub\AgileHub_Automation\ReadFiles\LucruCuFisiere\LucruCuFisiere\WriteLinesSample.txt");
+            //Console.WriteLine($"Textul nostru este: {readText}");
+
+            //citire din fisier 2
+            //var lines = System.IO.File.ReadLines(@"C:\Users\kstuchirenea\Desktop\AgileHub\NewGitHub\AgileHub_Automation\ReadFiles\LucruCuFisiere\LucruCuFisiere\WriteLinesSample.txt");
+            //foreach (var line in lines)
+            //{
+            //    Console.WriteLine(line);
+            //}
+
+            //string line;
+            //System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\kstuchirenea\Desktop\AgileHub\NewGitHub\AgileHub_Automation\ReadFiles\LucruCuFisiere\LucruCuFisiere\WriteLinesSample.txt");
+            //while (!(line = file.Read()).Contains("NumeScenariu"))
+            //{
+            //    Console.WriteLine(line);
+            //}
+
+            //var lines = System.IO.File.ReadLines(@"C:\Users\kstuchirenea\Desktop\AgileHub\NewGitHub\AgileHub_Automation\ReadFiles\LucruCuFisiere\LucruCuFisiere\WriteLinesSample.txt");
+            //foreach (var line in lines)
+            //{
+            //    if (line.Contains("NumeScenariu"))
+            //    {
+            //        Console.WriteLine(line);
+            //    }
+            //}
+            //file.Close();
+            //Console.WriteLine("Liniile au fost citite din fisier");
+            //Console.ReadKey();
+
+            //ScenarioDto.WriteXML();
+            //Console.WriteLine("Fisierul a fost creat");
+            //Console.ReadKey();
+
+            var myValue = ScenarioDto.ReadXML();
+            Console.WriteLine($"Numele Scenariului este: {myValue.TestCaseName}, Numele proprietatii 2 este: {myValue.TestInput}, proprietatea 3 este: {myValue.OtherInfo}");
+
+        }
+    }
+}
---
+++ ReadFiles/LucruCuFisiere/LucruCuFisiere/ScenarioDto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LucruCuFisiere
+{
+    public class ScenarioDto
+    {
+        public string TestCaseName { get; set; }
+        public string TestInput { get; set; }
+        public string OtherInfo { get; set; }
+
+        public static void WriteXML()
+        {
+            ScenarioDto myScenario = new ScenarioDto { TestCaseName = "Scenariul1", TestInput = "john.miller@bla.com", OtherInfo = "password123" };
+            //myScenario.TestCaseName = "Scenariul1";
+            //myScenario.TestInput = "john.miller@bla.com";
+            //myScenario.OtherInfo = "password123";
+            //System.Xml.Serialization.XmlSerializer writer = new System.Xml.Serialization.XmlSerializer(typeof(ScenarioDto));
+
+            var writer = new System.Xml.Serialization.XmlSerializer(typeof(ScenarioDto));
+            var writeFile = new System.IO.StreamWriter(@"C:\Users\kstuchirenea\Desktop\AgileHub\NewGitHub\AgileHub_Automation\ReadFiles\LucruCuFisiere\LucruCuFisiere\WriteXML.xml");
+
+            //var path = @"C:\Users\kstuchirenea\Desktop\AgileHub\NewGitHub\AgileHub_Automation\ReadFiles\LucruCuFisiere\LucruCuFisiere\WriteXML.xml";
+
+            //System.IO.FileStream file = System.IO.File.Create(path);
+            writer.Serialize(writeFile, myScenario);
+            writeFile.Close();
+
+        }
+
+        public static ScenarioDto ReadXML()
+        {
+            System.Xml.Serialization.XmlSerializer reader = new System.Xml.Serialization.XmlSerializer(typeof(ScenarioDto));
+            System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\kstuchirenea\Desktop\AgileHub\NewGitHub\AgileHub_Automation\ReadFiles\LucruCuFisiere\LucruCuFisiere\WriteXML.xml");
+            ScenarioDto myScenarioDto = (ScenarioDto)reader.Deserialize(file);
+            file.Close();
+            return myScenarioDto;
+        }
+
+    }
+}
